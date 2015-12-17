510fd33ac48b86476c78bc335c1d448a07db7e64
Philip Cameron-Smith <pjc@llnl.gov>
2015-12-16 21:20:04 -0500

    Code changes to implement linoz_mam3 outside of pp_linoz_mam3
    The general chemistry routines were modified to overwrite the
    tropospheric ozone values with the values from the fixed oxidant file
    for the MAM routines at each timestep.  This also implictly provides a
    tropospheric boundary condition for Linoz.
    
    Outside of the chemistry routines specific to linoz_mam3 (ie, outside
    pp_linoz_mam3) changes are needed to mimic the behavior of trop_mam3.
    This commonly occurs when there is code that is conditional on
    'trop_mam3', in which case linoz_mam3 it added to the IF test.
    
    linoz_mam4_mom was also added, when appropriate.
    However, linoz_mam4_mom has not been fully implemented or tested, yet.
    
    mo_chemini.F90            Pass chem_name to gas_phase_chemdr_inti.
    mo_gas_phase_chemdr.F90   Add chem_name as argument to gas_phase_chemdr_inti.
                              If linoz_mam3, then:  Initialize cnst_O3 in gas_phase_chemdr_inti.
                                                    Overwrite trop O3 with cnst_O3 in mo_gas_phase_chemdr.
    nucleate_ice.F90          If linoz_mam3, then:  Mimic behaviour of trop_mam3
    phys_control.F90          If linoz_mam3, then:  Mimic behaviour of trop_mam3
    modal_aer_opt.F90         If linoz_mam4_mom, then:  Mimic behaviour of trop_mam4
    
    [FCC] When specifying  -chem linoz_mam3 or -chem linoz_mam4_mom
    [AG-373]

--- components/cam/src/physics/cam/phys_control.F90
+++ components/cam/src/physics/cam/phys_control.F90
@@ -39,6 +39,7 @@ character(len=16) :: cam_physpkg          = unset_str  ! CAM physics package [ca
 character(len=32) :: cam_chempkg          = unset_str  ! CAM chemistry package [waccm_mozart | 
                                                        !  waccm_ghg | trop_mozart | trop_ghg | 
                                                        !  trop_bam | trop_mam3 | trop_mam7 | 
+                                                       !  linoz_mam3 | linoz_mam4_mom | 
                                                        !  super_fast_llnl | super_fast_llnl_mam3 | 
                                                        !  waccm_mozart_mam3 | none
 character(len=16) :: waccmx_opt           = unset_str  ! WACCMX run option [ionosphere | neutral | off
@@ -297,6 +298,8 @@ subroutine phys_ctl_readnl(nlfile)
    prog_modal_aero = (     cam_chempkg_is('trop_mam3') &
                       .or. cam_chempkg_is('trop_mam4') &
                       .or. cam_chempkg_is('trop_mam7') &
+                      .or. cam_chempkg_is('linoz_mam3') &
+                      .or. cam_chempkg_is('linoz_mam4_mom') &
                       .or. cam_chempkg_is('super_fast_llnl_mam3') &
                       .or. cam_chempkg_is('trop_mozart_mam3') &
                       .or. cam_chempkg_is('trop_strat_mam3') &
